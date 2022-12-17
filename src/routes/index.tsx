67567436949f40541c7d93464f3c4d2a52ce543e
Arnoldo Silva <analisi@gmail.com>
2022-12-17 14:44:49 -0300

    feat: add react router dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,16 @@
-export {};
+import {Routes, Route, Navigate} from 'react-router-dom';
+import Home from '../pages/home';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<Home />} />
+
+
+
+      <Route path="*" element={<Navigate to='/pagina-inicial' />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
